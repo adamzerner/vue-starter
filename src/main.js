4702d0990c96524069e8512e2b476e765a0808c8
Adam Zerner <adamzerner@protonmail.com>
2020-06-21 19:21:36 -0700

    Install Vuelidate

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import "@babel/polyfill";
 import "mutationobserver-shim";
 import Vue from "vue";
+import Vuelidate from "vuelidate";
 import "./plugins/bootstrap-vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+Vue.use(Vuelidate);
 Vue.config.productionTip = false;
 
 new Vue({
