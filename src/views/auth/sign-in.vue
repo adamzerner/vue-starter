c7f53ffd5e4128cdc281241546587b5bda0d4847
Adam Zerner <adamzerner@protonmail.com>
2020-06-21 19:21:47 -0700

    Add validation to sign in page

--- src/views/auth/sign-in.vue
+++ src/views/auth/sign-in.vue
@@ -1,21 +1,38 @@
 <template>
   <section>
     <page-header>Sign In</page-header>
-    <b-form>
+    <b-form v-on:submit.stop.prevent="submit" novalidate>
       <b-form-group label="Email">
-        <b-form-input v-model="email" type="email"></b-form-input>
+        <b-form-input
+          v-model="$v.form.email.$model"
+          v-bind:state="validateState('email')"
+          type="email"
+        ></b-form-input>
+        <b-form-invalid-feedback>
+          <span v-if="!$v.form.email.required">
+            You forgot to include your email.
+          </span>
+          <span v-else-if="!$v.form.email.email">
+            That is an invalid email address.
+          </span>
+        </b-form-invalid-feedback>
       </b-form-group>
       <b-form-group label="Password">
         <b-form-input
           v-if="!showPassword"
-          v-model="password"
+          v-model="$v.form.password.$model"
+          v-bind:state="validateState('password')"
           type="password"
         ></b-form-input>
         <b-form-input
           v-if="showPassword"
-          v-model="password"
+          v-model="$v.form.password.$model"
+          v-bind:state="validateState('password')"
           type="text"
         ></b-form-input>
+        <b-form-invalid-feedback>
+          You forgot to include your password.
+        </b-form-invalid-feedback>
         <template v-slot:description>
           <b-form-checkbox v-model="showPassword">
             <span>Show password</span>
@@ -24,7 +41,7 @@
       </b-form-group>
       <b-button type="submit" variant="primary" block>Sign in</b-button>
       <hr />
-      <b-form-checkbox v-model="keepMeSignedIn">
+      <b-form-checkbox v-model="form.keepMeSignedIn">
         Keep me signed in on this computer
       </b-form-checkbox>
       <hr />
@@ -40,6 +57,7 @@
 
 <script>
 import PageHeader from "@/components/page-header/page-header.vue";
+import { required, email } from "vuelidate/lib/validators";
 
 export default {
   name: "sign-in",
@@ -48,11 +66,46 @@ export default {
   },
   data() {
     return {
-      email: "",
-      password: "",
+      form: {
+        email: "",
+        password: "",
+        keepMeSignedIn: true,
+      },
       showPassword: false,
-      keepMeSignedIn: true,
+      submitAttempted: false,
     };
+  },
+  validations: {
+    form: {
+      email: {
+        required,
+        email,
+      },
+      password: {
+        required,
+      },
+    },
+  },
+  methods: {
+    validateState(name) {
+      const { $dirty, $error } = this.$v.form[name];
+
+      if (this.submitAttempted && $dirty && $error) {
+        return false;
+      }
+
+      return null;
+    },
+    submit() {
+      this.submitAttempted = true;
+      this.$v.form.$touch();
+
+      if (this.$v.form.$anyError) {
+        return;
+      }
+
+      alert("Form submitted!");
+    },
   },
 };
 </script>
