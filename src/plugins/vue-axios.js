3d2d6053b9e54eca9ffc56e024ab156504e8b7d2
Adam Zerner <adamzerner@protonmail.com>
2020-06-29 11:51:45 -0700

    Update vue-axios.js

--- src/plugins/vue-axios.js
+++ src/plugins/vue-axios.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import axios from "axios";
 import VueAxios from "vue-axios";
 
+console.log(`process.env.VUE_APP_BASE_URL: ${process.env.VUE_APP_BASE_URL}`);
 axios.defaults.baseURL = process.env.VUE_APP_BASE_URL;
 axios.interceptors.request.use(
   (config) => {
