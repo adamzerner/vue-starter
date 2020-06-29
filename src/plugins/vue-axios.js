0661189dafc9ab37b93ca93de21133cafc179763
Adam Zerner <adamzerner@protonmail.com>
2020-06-29 12:07:27 -0700

    Remove console.log

--- src/plugins/vue-axios.js
+++ src/plugins/vue-axios.js
@@ -2,7 +2,6 @@ import Vue from "vue";
 import axios from "axios";
 import VueAxios from "vue-axios";
 
-console.log(`process.env.VUE_APP_BASE_URL: ${process.env.VUE_APP_BASE_URL}`);
 axios.defaults.baseURL = process.env.VUE_APP_BASE_URL;
 axios.interceptors.request.use(
   (config) => {
